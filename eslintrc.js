d71b842d59879b61752702d6a23fe386bf37f80c
gyuri <rbfl7645@naver.com>
2025-06-14 21:03:09 +0900

    기본세팅

---
+++ eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // 커스터마이징 가능
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+
+const Container = styled.div `
+  text-align: center;
+`
+
+function App() {
+  return (
+    <Container>
+      {/*<div className="App">*/}
+      <h1>Hello, React + TypeScript!</h1>
+      {/*</div>*/}
+    </Container>
+  );
+}
+
+export default App;
